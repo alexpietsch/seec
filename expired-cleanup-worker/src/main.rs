b3b577cf0fa93d1bf2949bc2bf41ad1ada28bbb2
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2025-07-06 10:01:16 +0200

    chore: update dependencies and improve error handling in main logic

--- expired-cleanup-worker/src/main.rs
+++ expired-cleanup-worker/src/main.rs
@@ -1,6 +1,6 @@
-use postgres::{Client, NoTls, Error};
-use std::{thread, time::Duration};
 use chrono::Utc;
+use postgres::{Client, Error, NoTls};
+use std::{thread, time::Duration};
 
 fn main() -> Result<(), Error> {
     let connection_string = std::env::args().nth(1).expect("Missing Database URL: No DB URL in format `postgresql://<user>:<password>@<host>:<port>/<database>` provided");
@@ -12,16 +12,20 @@ fn main() -> Result<(), Error> {
                 println!("{} - Removed {} rows.", now, rows_deleted);
             }
             Err(e) => {
-                eprintln!("Error deleting rows: {}. Retrying in 10 seconds.", e);
+                eprintln!("Error deleting rows: {}. Retrying in 30 seconds.", e);
             }
         }
 
-        thread::sleep(Duration::from_secs(10));
+        thread::sleep(Duration::from_secs(30));
     }
 }
 
 fn clean_expired_entries(connection_string: &str) -> Result<u64, Error> {
-    let mut client = Client::connect(connection_string, NoTls)?;
-    let rows_deleted = client.execute("DELETE FROM seec.secret WHERE auto_expire_at < NOW();", &[])?;
+    let mut client = Client::connect(connection_string, NoTls).map_err(|e| {
+        eprintln!("Failed to connect to database: {}", e);
+        e
+    })?;
+    let rows_deleted =
+        client.execute("DELETE FROM seec.secret WHERE auto_expire_at < NOW();", &[])?;
     Ok(rows_deleted)
 }
