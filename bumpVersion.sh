252c3bdbd112354577a3e9747afb7d949b11b9cd
alexpietsch <54153428+alexpietsch@users.noreply.github.com>
2025-07-06 10:09:08 +0200

    feat: update version to 0.3.0 in package.json, pom.xml, Cargo.toml, and seec.config

--- bumpVersion.sh
+++ bumpVersion.sh
@@ -3,9 +3,12 @@
 VERSION=$(grep 'version:' seec.config | awk '{print $2}')
 
 echo "Bumping package.json version to $VERSION"
-sed -i -E "s/\"version\": \"[^\"]+\"/\"version\": \"$VERSION\"/" ./frontend/package.json
+sed -i '' -E "s/\"version\": \"[^\"]+\"/\"version\": \"$VERSION\"/" ./frontend/package.json
 
 echo "Bumping pom.xml version to $VERSION"
-sed -i -E "s/<version>[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT<\/version>/<version>${VERSION}-SNAPSHOT<\/version>/" ./server/pom.xml
+sed -i '' -E "s/<version>[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT<\/version>/<version>${VERSION}-SNAPSHOT<\/version>/" ./server/pom.xml
 
-echo "Version updated to $VERSION in package.json and pom.xml"
+echo "Bumping Cargo.toml version to $VERSION"
+sed -i '' -E "s/version = \"[^\"]+\"/version = \"$VERSION\"/" ./expired-cleanup-worker/Cargo.toml
+
+echo "Version updated to $VERSION in package.json, pom.xml and Cargo.toml"
